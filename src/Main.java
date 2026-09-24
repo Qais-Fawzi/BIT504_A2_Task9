9f427c65288432bc49192c92110c259553fdd2fe
Qais-Fawzi <qfawzi7@gmail.com>
2026-09-24 16:06:25 +1200

    Add initial Java project with Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+   /*
+    * BIT504 Programming 1 - Assessment 2, Task 9
+    * Name: Qais Fawzi  
+    * Student number: 5159690
+    */
+   public class Main {
+       public static void main(String[] args) {
+           System.out.println("BIT504 Assessment 2 - Task 9");
+       }
+   }
