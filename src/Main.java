570713020d7538e0319a7c68a9694b7bc02f8dc3
Qais-Fawzi <qfawzi7@gmail.com>
2026-09-24 16:15:04 +1200

    Add welcome message to program output

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@
    public class Main {
        public static void main(String[] args) {
            System.out.println("BIT504 Assessment 2 - Task 9");
+           System.out.println("Welcome to my version control demonstration.");
        }
    }
